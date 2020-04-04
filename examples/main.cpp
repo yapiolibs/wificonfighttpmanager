d95fdc55d3ca235854548ebbae15e6ff8d5a5922
Dipl.-Ing. Raoul Rubien, BSc <rubienr@gmail.com>
2020-04-04 19:02:54 +0200

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,12 @@
+#include <Display.h>
 #include <WifiConfigOrFallbackAccesspointManager.h>
+
+Display display; 
 
 void setup()
 {
+    display.setup();
+    
     WiFi.mode(WIFI_AP);
     WifiConfigOrFallbackAccesspointManager foo(display);
 }
